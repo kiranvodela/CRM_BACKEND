140f5eca2fe20b9412fc6f74a0d8166aa6041dac
kiranvodela <iam.saikiran527@gmil.com>
2026-06-06 16:01:53 +0530

    env changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Ensure no trailing slash
+  origin: process.env.FRONTEND_URL || 'https://crmbyvsk.netlify.app', // Ensure no trailing slash
   credentials: true
 }));
 
